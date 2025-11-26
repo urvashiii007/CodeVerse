0f0f655115dbfe31d10b34f3cc83592a234b2fe0
Urvashi Singh <us229253@gmail.com>
2025-11-27 01:24:45 +0530

    update: improved editor & UI

--- client/src/types/setting.ts
+++ client/src/types/setting.ts
@@ -1,23 +1,4 @@
-/*
-interface Settings {
-    theme: string
-    language: string
-    fontSize: number
-    fontFamily: string
-    showGitHubCorner: boolean
-}
 
-interface SettingsContext extends Settings {
-    setTheme: (theme: string) => void
-    setLanguage: (language: string) => void
-    setFontSize: (fontSize: number) => void
-    setFontFamily: (fontFamily: string) => void
-    setShowGitHubCorner: (showGitHubCorner: boolean) => void
-    resetSettings: () => void
-}
-
-export { Settings, SettingsContext }
-*/
 interface Settings {
     theme: string
     language: string
